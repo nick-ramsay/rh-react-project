0d8d54c494c0477644176bda1a5778319d332530
Nick Ramsay <NickRamsay1989@gmail.com>
2020-02-10 21:55:34 +1100

    Added dropdown menu

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -15,7 +15,28 @@ class Comments extends Component {
                 <div className="container-fluid pt-4">
                     <div className="row button-row">
                         <div className="col-md-12">
-                            <button id="alert-btn" className="btn btn-custom float-right m-1"><img className="icon-image" src={AlertIcon} /></button>
+                            <div className="dropdown">
+                                <button id="alert-btn" className="btn btn-custom float-right m-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img className="icon-image" src={AlertIcon} /></button>
+                                <div class="dropdown-menu mt-1 mr-2" aria-labelledby="dropdownMenuButton">
+                                    <div className="col-md-12">
+                                        <div className="row comment-row m-1">
+                                            <div className="col-md-2">
+                                                <p>img</p>
+                                            </div>
+                                            <div className="col-md-10">
+                                                <p className="comment-details">Frank Greeff</p>
+                                                <p className="comment-details">Also, the address is 79/104 New Order Road not 78</p>
+                                                <p className="comment-details"><span>2 minutes ago</span><span> | </span><span>Mark as Seen</span></p>
+                                            </div>
+                                        </div>
+                                        <div className="row">
+                                            <div className="col-md-12 text-center">
+                                                <button className="btn btn-success">Acknowledge</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                     <div className="row">
