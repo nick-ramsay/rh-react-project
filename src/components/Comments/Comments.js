a0b0fa4777679ccbb2f093b56fd1572890f7c97e
Nick Ramsay <NickRamsay1989@gmail.com>
2020-02-10 22:55:00 +1100

    Comments now reflect JSON data

--- src/components/Comments/Comments.js
+++ src/components/Comments/Comments.js
@@ -1,54 +1,57 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from 'react';
 import AlertIcon from "../../images/alert_icon.png";
 import PropertyImage from "../../images/property.jpg";
+import CommentData from "../../data/comment-data.json";
 import "./style.css";
 
-class Comments extends Component {
-    state = {
+const Comments = () => {
 
-    }
+    const [comments, setComments] = useState([]);
 
+    useEffect(() => {
+        setComments(CommentData);
+    });
 
-    render() {
-        return (
-            <div>
-                <div className="container-fluid pt-4">
-                    <div className="row button-row">
-                        <div className="col-md-12">
-                            <div className="dropdown">
-                                <button id="alert-btn" className="btn btn-custom float-right m-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img className="icon-image" src={AlertIcon} /></button>
-                                <div class="dropdown-menu mt-1 mr-2" aria-labelledby="dropdownMenuButton">
-                                    <div className="col-md-12">
-                                        <div className="row comment-row m-1">
+    return (
+        <div>
+            <div className="container-fluid pt-4">
+                <div className="row button-row">
+                    <div className="col-md-12">
+                        <div className="dropdown">
+                            <button id="alert-btn" className="btn btn-custom float-right m-1" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"><img className="icon-image" src={AlertIcon} /></button>
+                            <div class="dropdown-menu mt-1 mr-2 comment-dropdown" aria-labelledby="dropdownMenuButton">
+                                <div className="col-md-12">
+                                    {comments.map(comment => (
+                                        < div className="row comment-row m-1 pb-1 pt-1" >
                                             <div className="col-md-2">
-                                                <p>img</p>
+                                                <img className="img-fluid comment-user-img" src={comment.user.image.thumb_url} />
                                             </div>
                                             <div className="col-md-10">
-                                                <p className="comment-details">Frank Greeff</p>
-                                                <p className="comment-details">Also, the address is 79/104 New Order Road not 78</p>
+                                                <p className="comment-details" id="comment-username">{comment.user.first_name} {comment.user.last_name}</p>
+                                                <p className="comment-details">{comment.body}</p>
                                                 <p className="comment-details"><span>2 minutes ago</span><span> | </span><span>Mark as Seen</span></p>
                                             </div>
                                         </div>
-                                        <div className="row">
-                                            <div className="col-md-12 text-center">
-                                                <button className="btn btn-success">Acknowledge</button>
-                                            </div>
+                                    ))
+                                    }
+                                    <div className="row">
+                                        <div className="col-md-12 text-center">
+                                            <button className="btn btn-success">Acknowledge</button>
                                         </div>
                                     </div>
                                 </div>
                             </div>
                         </div>
                     </div>
-                    <div className="row">
-                        <div className="col-md-6 offset-md-3 mt-3">
-                            <img className="img-fluid property-image" src={PropertyImage} />
-                        </div>
+                </div>
+                <div className="row">
+                    <div className="col-md-6 offset-md-3 mt-3">
+                        <img className="img-fluid property-image" src={PropertyImage} />
                     </div>
                 </div>
             </div>
-        )
-    }
-
+        </div >
+    )
 }
 
 export default Comments;
